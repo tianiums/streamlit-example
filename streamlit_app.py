3196a03ff2f8fcf6a9bdc082b1a27a29979a9864
tianiums <132368238+tianiums@users.noreply.github.com>
2023-12-24 08:33:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,9 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Seftian Setia
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+ini adalah Dashboard sederhana mengingat waktu tinggal 1 hari lagi
 """
 
 
