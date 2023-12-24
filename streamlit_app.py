d0364bb883a6a7a0ae66813b6990056bd6b75f8c
tianiums <132368238+tianiums@users.noreply.github.com>
2023-12-24 08:25:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,20 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+sns.set(style='dark')
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+st.title('E-Bike Sharing Analysis')
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+with tab1:
+  st.header("Hari atau Jam dengan penyewa terbanyak?")
+  st.image('https://github.com/tianiums/streamlit-example/blob/master/jam%20efektif.png')
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+with tab2:
+  st.header("Musim Terbaik dan Cuaca yang mendukung?")
+  st.image('https://github.com/tianiums/streamlit-example/blob/master/Musim%20terbaik.png')
+  st.image('https://github.com/tianiums/streamlit-example/blob/master/berdasar%20cuaca.png')
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+with tab3:
+  st.header("Workday atau Weekend yang terbanyak penyewanya?")
+  st.image('https://github.com/tianiums/streamlit-example/blob/master/rata%20arat%20pemakaian.png')
